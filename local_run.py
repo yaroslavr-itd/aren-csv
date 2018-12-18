42a53cbd30ba45b97a527cb804cf64640e0fcc1f
Yaroslav R <yaroslav.r@it-dimension.com>
2018-12-18 14:01:20 +0200

    Fix naming.

--- local_run.py
+++ local_run.py
@@ -1,11 +1,11 @@
 import os
 
-import aren_csv_app
+import qepccc_app
 
 HOST = os.getenv('QEPCCC_HOST', 'localhost')
 PORT = os.getenv('QEPCCC_PORT', 8080)
 DEBUG = os.getenv('QEPCCC_DEBUG', True)
 
-APP = aren_csv_app.create_app()
+APP = qepccc_app.create_app()
 
 APP.run(host=HOST, port=PORT, debug=DEBUG)
